f2f95866732c0231d262423337bf73124158c95e
Vladimir Ceban <v3ceban@proton.me>
2024-01-29 14:36:21 -0800

    lab1

---
+++ lab2/src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <>
+      <h1>Hello world</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum sequi
+        quos beatae ut deserunt architecto. Quidem iste illum quis. Minima.
+      </p>
+    </>
+  );
+}
+
+export default App;
